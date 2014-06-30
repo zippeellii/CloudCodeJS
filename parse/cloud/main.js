87d63b600c7e9b7bfdb41436123bfaa01c4e3ccf
Jonathan Nilsfors <jonathan.nilsfors@ctk.se>
2014-06-30 10:14:04 +0200

    Initial files to cloud code

---
+++ parse/cloud/main.js
@@ -0,0 +1,20 @@
+
+// Use Parse.Cloud.define to define as many cloud functions as you want.
+// For example:
+Parse.Cloud.define("hello", function(request, response) {
+  response.success("Hello world!");
+});
+
+Parse.Cloud.define(      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk')));
+
+Parse.FacebookUtils.init({    
+	appId      : '1432414407034244',
+    status     : true,
+    xfbml      : true,
+    version    : 'v2.0',});
